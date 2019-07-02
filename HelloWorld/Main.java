49a5baa4ae74da902bff3e901a1a686f38c71643
govkriss <36509649+govkriss@users.noreply.github.com>
2019-07-02 14:50:40 +0530

    Update Main.java

--- HelloWorld/Main.java
+++ HelloWorld/Main.java
@@ -5,5 +5,6 @@ public class Main
     public static void main(String[] args)
     {
         System.out.println("Hello world");
+        System.out.println("This is my first Git Hub and Jenkins program");
     }
 }
